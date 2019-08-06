e07b0cea6aa76e3f3acca87b0854a9c76174addc
Osman Mejia <osman.mejia@grupoleitz.com>
2019-08-06 14:27:41 -0600

    pong

--- src/Kata.Spec/when_feeding_the_monkey.cs
+++ src/Kata.Spec/when_feeding_the_monkey.cs
@@ -18,17 +18,27 @@ namespace Kata.Spec
     }
 
     public class when_user_input_is_empty
+         {
+             Establish _context = () => { _systemUnderTest = new Calculator(); };
+     
+             Because of = () => { _result = _systemUnderTest.Add(); };
+     
+             It should_return_zero = () => { _result.Should().Be(0); };
+             static Calculator _systemUnderTest;
+             static int _result;
+         }
+    
+    public class when_user_input_is_one_number
     {
         Establish _context = () => { _systemUnderTest = new Calculator(); };
 
-        Because of = () => { _result = _systemUnderTest.Add(); };
+        Because of = () => { _result = _systemUnderTest.Add("3"); };
 
-        It should_return_zero = () => { _result.Should().Be(0); };
+        It should_return_the_same_number = () => { _result.Should().Be(3); };
         static Calculator _systemUnderTest;
         static int _result;
     }
 
-//    1. Given the user input is empty when calculating the sum then it should return zero.
 //    2. Given the user input is one number when calculating the sum then it should return the same number. (example "3" should equal 3)
 //    3. Given the user input is two numbers when calculating the sum then it should return the sum of those numbers. (example "1,2" should equal 3)
 //    4. Given the user input is an unknown amount of numbers when calculating the sum then it should return the sum of all the numbers. (example "1,2,3" should equal 6)
